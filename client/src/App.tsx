ec68c76463688a4cc8be9f38e629b1a9e69dba95
Gaeel Bradshaw <gaeel@spaceshipsin.space>
2024-04-17 12:28:12 +0200

    feat: handle initial load and fetch errors

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,27 +2,85 @@ import React from 'react';
 import { useEffect, useState } from 'react';
 import './App.css';
 
+enum StateType {
+  Unset,
+  Error,
+  State,
+}
+
+type Unset = {
+  type: StateType.Unset,
+}
+
+type State = {
+  type: StateType.State,
+  count: number;
+}
+
+type StateError = {
+  type: StateType.Error,
+  error: Error
+}
+
+type NetworkState = 
+    | State
+    | StateError
+    | Unset;
+
 
 function MyButton() {
-  const [count, setCount] = useState(0);
+  const [state, setState] = useState({type: StateType.Unset} as NetworkState);
 
   useEffect(() => {
     fetch("/get-count")
       .then((res) => res.json())
-      .then((data) => setCount(data.counter));
+      .then((data) => setState({
+          type: StateType.State,
+          count: data.count
+        }))
+      .catch(error => 
+        setState({
+          type: StateType.Error,
+          error: error
+        }));
   }, []);
 
   function handleClick() {
     fetch("/bump")
       .then((res) => res.json())
-      .then((data) => setCount(data.counter));
+      .then((data) => setState({
+          type: StateType.State,
+          count: data.count
+        }))
+      .catch(error => 
+        setState({
+          type: StateType.Error,
+          error: error
+        }));
   }
 
-  return (
-    <button onClick={handleClick}>
-      Clicked {count} times
-    </button>
-  );
+  switch (state.type) {
+    case StateType.Unset:
+      return <div><p>Loading...</p></div>
+    case StateType.Error:
+      return (
+      <div>
+        <p>Could not load current count...</p>
+        <button onClick={handleClick}>
+          Try again?
+        </button>
+      </div>
+      )
+    default:
+      return (
+        <div>
+          <p>Button was clicked {state.count} times</p>
+          <button onClick={handleClick}>
+            bump
+          </button>
+        </div>
+      )
+  }
 }
 
 function App() {
--- server/src/main.rs
+++ server/src/main.rs
@@ -2,31 +2,31 @@ use actix_web::{web, App, HttpServer};
 use serde::Serialize;
 use std::sync::Mutex;
 struct AppStateWithCounter {
-    counter: Mutex<i32>, // <- Mutex is necessary to mutate safely across threads
+    count: Mutex<i32>, // <- Mutex is necessary to mutate safely across threads
 }
 
 #[derive(Serialize)]
 struct SerializableAppState {
-    counter: i32,
+    count: i32,
 }
 
 async fn bump(data: web::Data<AppStateWithCounter>) -> String {
-    let mut counter = data.counter.lock().unwrap(); // <- get counter's MutexGuard
-    *counter += 1; // <- access counter inside MutexGuard
+    let mut count = data.count.lock().unwrap(); // <- get counter's MutexGuard
+    *count += 1; // <- access counter inside MutexGuard
 
-    serde_json::to_string(&SerializableAppState { counter: *counter }).unwrap()
+    serde_json::to_string(&SerializableAppState {count: *count}).unwrap()
 }
 
 async fn get_count(data: web::Data<AppStateWithCounter>) -> String {
-    let counter = data.counter.lock().unwrap(); // <- get counter's MutexGuard
-    serde_json::to_string(&SerializableAppState { counter: *counter }).unwrap()
+    let count = data.count.lock().unwrap(); // <- get counter's MutexGuard
+    serde_json::to_string(&SerializableAppState { count: *count }).unwrap()
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     // Note: web::Data created _outside_ HttpServer::new closure
     let counter = web::Data::new(AppStateWithCounter {
-        counter: Mutex::new(0),
+        count: Mutex::new(0),
     });
 
     HttpServer::new(move || {
